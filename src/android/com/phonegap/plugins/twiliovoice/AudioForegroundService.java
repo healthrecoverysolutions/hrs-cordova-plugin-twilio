d212261df1b231382af6528918d75bd657b2663a
NehalDutta <nehaldutta7@gmail.com>
2025-02-10 23:11:37 +0530

    Fixed DEV-20001 [KM][PCMT] Voice call: Patient can hear clinician in background, but clinician cannot hear patient until the app is reopened (#7)
    * Changes for DEV-20001 to make audio calls functional in background
    
    * startForegroundservice is available since android 8 + thus applying this check
    
    ---------
    
    Co-authored-by: Nehal <ndutta@healthrecoverysolutions.com>

---
+++ src/android/com/phonegap/plugins/twiliovoice/AudioForegroundService.java
@@ -0,0 +1,49 @@
+package com.phonegap.plugins.twiliovoice;
+
+import android.app.*;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import androidx.core.app.NotificationCompat;
+
+public class AudioForegroundService extends Service {
+
+    private static boolean isServiceRunning = false;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        isServiceRunning = true;
+        startForeground(1, createNotification());
+    }
+
+    public static boolean isRunning() {
+        return isServiceRunning;
+    }
+
+    private Notification createNotification() {
+        String channelId = "ForegroundServiceChannel";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                channelId, "Foreground Service", NotificationManager.IMPORTANCE_LOW);
+            getSystemService(NotificationManager.class).createNotificationChannel(channel);
+        }
+
+        return new NotificationCompat.Builder(this, channelId)
+            .setContentTitle("Voice call")
+            .setContentText("Voice call is in progress")
+            .setSmallIcon(android.R.drawable.ic_btn_speak_now)
+            .build();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        isServiceRunning = false;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
